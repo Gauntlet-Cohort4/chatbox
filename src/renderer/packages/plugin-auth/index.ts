b38ee095f6012085a204d1bd44d35087719770b8
FpSilSha <Faperez38TheSilDev@gmail.com>
2026-04-02 23:47:53 -0500

    feat(plugins): add plugin-auth barrel export
    Re-exports all PKCE utilities and OAuth flow functions from index.

---
+++ src/renderer/packages/plugin-auth/index.ts
@@ -0,0 +1,2 @@
+export { generateCodeVerifier, generateCodeChallenge, buildAuthorizationUrl, exchangeCodeForToken, refreshAccessToken } from './pkce'
+export { startOAuthFlow, ensureValidToken } from './flow'
