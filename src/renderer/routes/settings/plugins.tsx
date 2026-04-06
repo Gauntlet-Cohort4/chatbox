036d34f85e0cae8171bdc47d7f1659f98a0c58c6
FpSilSha <Faperez38TheSilDev@gmail.com>
2026-04-06 02:04:58 -0500

    feat(marketplace): mount MarketplacePanel above PluginStorePanel in Apps tab

--- src/renderer/routes/settings/plugins.tsx
+++ src/renderer/routes/settings/plugins.tsx
@@ -1,10 +1,19 @@
+import { Stack } from '@mantine/core'
 import { createFileRoute } from '@tanstack/react-router'
+import { MarketplacePanel } from '@/components/settings/plugins/MarketplacePanel'
 import { PluginStorePanel } from '@/components/settings/plugins/PluginStorePanel'
+import { useDemoStore } from '@/stores/demoStore'
 
 export const Route = createFileRoute('/settings/plugins')({
 	component: RouteComponent,
 })
 
 export function RouteComponent() {
-	return <PluginStorePanel />
+	const role = useDemoStore((s) => s.demoRole)
+	return (
+		<Stack gap="lg" p="md">
+			<MarketplacePanel role={role} />
+			<PluginStorePanel />
+		</Stack>
+	)
 }
